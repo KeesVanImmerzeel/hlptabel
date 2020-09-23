b20c2a11a4837093050498a1560e52f742178b57
KeesVanImmerzeel <idodoes@xs4all.nl>
2020-09-23 15:02:13 +0200

    hlptabel.r updated (2)

--- R/hlptabel.r
+++ R/hlptabel.r
@@ -31,4 +31,23 @@
 #' @docType package
 #' @name hlptabel
 #'
+#' @importFrom magrittr %<>%
+#' @importFrom magrittr %>%
+#'
+#' @importFrom dplyr filter
+#' @importFrom dplyr arrange
+#' @importFrom dplyr desc
+#'
+#' @importFrom stats na.omit
+#'
+#' @importFrom ggplot2 ggplot
+#' @importFrom ggplot2 aes
+#' @importFrom ggplot2 geom_point
+#' @importFrom ggplot2 geom_abline
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 labs
+#' @importFrom ggplot2 ggtitle
+#' @importFrom ggplot2 theme
+#' @importFrom ggplot2 element_blank
+#' @importFrom ggplot2 element_text
 NULL
