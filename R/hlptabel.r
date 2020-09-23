84cfeb928f3e6ec52a3db9f436b1a3cca61b545b
KeesVanImmerzeel <idodoes@xs4all.nl>
2020-09-23 14:57:32 +0200

    hlptabel.r updated

--- R/hlptabel.r
+++ R/hlptabel.r
@@ -1,7 +1,34 @@
-#' hlptabel: a package
+#' hlptabel: Calculate reduction in crop production caused by waterlogging and drought.
+#'
+#' Based on the publication "De invloed van de waterhuishouding op de landbouwkundige produktie".
+#' Rapport van de werkgroep HELP-tabel, Mededelingen Landinrichtingsdienst 176 (1987).
+#' \href{https://edepot.wur.nl/188152}{Rapport van de werkgroep HELP-tabel}
+#'
+#' Approximate analytical formulas are used to calculate the reduction in crop productions.
+#'
+#' This package exports no sample data sets/objects
+#'
+#' This package exports the following functions:
+#'
+#' * \code{\link{ht_reduction}}
+#'
+#' * \code{\link{ht_soilnr_to_HELPnr}}
+#'
+#' * \code{\link{ht_bofek_to_HELPnr}}
+#'
+#' * \code{\link{ht_soil_unit_to_HELPnr}}
+#'
+#' * \code{\link{ht_soil_units}}
+#'
+#' * \code{\link{ht_bofek_numbers}}
+#'
+#' * \code{\link{ht_HELPnr_to_HELPcode}}
+#'
+#' * \code{\link{ht_tab_calc_values}}
+#'
+#' * \code{\link{ht_plot_tab_calc_values}}
 #'
 #' @docType package
 #' @name hlptabel
-#'
 #'
 NULL
