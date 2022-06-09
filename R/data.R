10e6bef72f49bb06defac42d289e456e5a185998
Immerzeel <kees.vanimmerzeel@sweco.nl>
2022-06-09 22:59:50 +0200

    Correction of minor flaw in Landuse data.

--- R/data.R
+++ R/data.R
@@ -3,11 +3,12 @@
 #' Raster map of 1987 HELP codes for the Netherlands, based on the Bofek2020 raster map.
 #' Resolution 25x25m.
 #'
+#' @source \url{https://www.wur.nl/nl/show/bodemfysische-eenhedenkaart-bofek2020.htm}
 "HELP_map_NL2020"
 
 #' landuse map (units gras=1, akker=2) for the Netherlands.
 #'
 #' Raster map (resolution 25x25m) of landuse (2021) based on the TOP25raster_GEOTIFF dataset 2021.
 #'
-#'
+#' @source \url{https://www.pdok.nl/downloads/-/article/basisregistratie-topografie-brt-historie}
 "landuse_map_NL2021"
--- data-raw/Bofek2020/HELP_map_NL2020.R
+++ data-raw/Bofek2020/HELP_map_NL2020.R
@@ -7,6 +7,7 @@ rcl <- "BOFEK2020_HELP.csv" %>% file.path("data-raw","Bofek2020",.) %>% read.tab
 x <- "BOFEK2020.tif" %>% file.path("data-raw","Bofek2020",.) %>% terra::rast()
 HELP_map_NL2020 <- x %>% terra::classify(rcl, others=NA )
 
+# Use result.
 # SpatRaster breaks when saved as an R object, so convert to raster format before writing to rda-file.
 HELP_map_NL2020%<>% raster::raster()
 usethis::use_data(HELP_map_NL2020, internal = FALSE, overwrite=TRUE, version=2)
