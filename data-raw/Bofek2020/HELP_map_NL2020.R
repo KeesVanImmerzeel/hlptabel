76ec5f9e0a83c6fbd062414d76d12423612506d2
Immerzeel <kees.vanimmerzeel@sweco.nl>
2022-06-10 21:25:58 +0200

    HELP numbers 71 and 72 renumbered in HELP_map_NL2020

--- data-raw/Bofek2020/HELP_map_NL2020.R
+++ data-raw/Bofek2020/HELP_map_NL2020.R
@@ -7,6 +7,15 @@ rcl <- "BOFEK2020_HELP.csv" %>% file.path("data-raw","Bofek2020",.) %>% read.tab
 x <- "BOFEK2020.tif" %>% file.path("data-raw","Bofek2020",.) %>% terra::rast()
 HELP_map_NL2020 <- x %>% terra::classify(rcl, others=NA )
 
+# In the HELP-table (1987) the HELP numbers 71 and 72 where not included.
+# Renumber these HELP numbers to the most similar soil codes in the HELP-table (1987).
+# HELP-2005. stowa rapport 2005/16, page 16.
+# 'Uitbreiding en actualisering van de HELP-tabellen ten behoeve van het Waternood-instrumentarium'.
+m <- c(71, 67,
+       72, 60)
+rcl <- matrix(m, ncol=2, byrow=TRUE)
+HELP_map_NL2020 <-  terra::classify(HELP_map_NL2020, rcl )
+
 # Use result.
 # SpatRaster breaks when saved as an R object, so convert to raster format before writing to rda-file.
 HELP_map_NL2020%<>% raster::raster()
